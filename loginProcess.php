71304650d1ff830dd1f21641b61c76d9f45df9a3
imsunhao <313696376@qq.com>
2016-09-29 08:14:45 +0800

    9.29 8:05

--- loginProcess.php
+++ loginProcess.php
@@ -1,11 +1,18 @@
 <?php
 $id=$_POST['id'];
 $passwd=$_POST['password'];
-if($id=="100"&&passwd=="123"){
-header("Location:stuManager.php");
-exit();
-}else{
-echo "fail";
-header("Location:login.php?errnum=1");
+$conn=mysql_connect("localhost","root","");
+if(!$conn){die("connect fail".mysql_errno());}
+mysql_query("set names utf8",$conn) or die (mysql_errno());
+mysql_select_db("stuadminsystem",$conn) or die (mysql_errno());
+$sql="select * from admin where id=$id ";
+$res=mysql_query($sql,$conn);
+if($row=mysql_fetch_assoc($res)){if($row['password']==md5($passwd)){
+$name=$row['name'];
+header("Location:stuManager.php?name=$name");}}else{header("Location:login.php?errnum=1");
+;
 }
+mysql_free_result($res);
+mysql_close($conn);
+exit();
 ?>
--- stuManager.php
+++ stuManager.php
@@ -1,4 +1,18 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+</head>
+<body>
 <?php
-echo "ok!";
+echo "hello ".$_GET['name']." login success!";
 echo "<br><a href='login.php'>back to login</a></br>";
 ?>
+<h1>Main Page</h1>
+<a href='stuList.php'>Manaeger Student</a>
+<br/>
+<a href='addstu.php'>Add Student</a>
+<br />
+<a href='#'>Find Student</a>
+<br />
+</body>
+</html>
